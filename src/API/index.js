230d0ffb0b5f72a4c650376788829cbdcd2cfe06
abraham <abrahamsuarezolay@gmail.com>
2023-10-13 16:05:26 +0100

    Firebase and firebase auth installed

---
+++ src/API/index.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0f4daYdpHXTffpfb43mWsrf3pVI-K02A",
+  authDomain: "mockbank-f67f8.firebaseapp.com",
+  projectId: "mockbank-f67f8",
+  storageBucket: "mockbank-f67f8.appspot.com",
+  messagingSenderId: "187687114706",
+  appId: "1:187687114706:web:6f1db293e4246b4649da0b",
+  measurementId: "G-E8572R55Y0"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,19 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export function Login(){
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
 
     return(
     <div>
